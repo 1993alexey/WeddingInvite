439932971fca77c5b46b9cb7bf3c3d978716e271
Alex <1993alexey@gmail.com>
2019-03-03 17:59:54 -0700

    Finished all routes

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,51 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const serviceAccount = require('./account-key');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((req, res) => {
-//  res.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+
+const db = admin.firestore();
+const attendeesRef = db.collection('guests');
+const wishesRef = db.collection('wishes')
 
 exports.attendees = functions.https.onRequest((req, res) => {
 	if (req.method === "GET") {
-		res.send(
-			[
-                {
-                    nameFirst: 'Yuan-Ting',
-                    nameLast: 'Lu',
-                },
-                {
-                    nameFirst: 'Alex',
-                    nameLast: 'Shnyrov',
-                }
-            ]
-		);
+        let attendees = [];
+
+        attendeesRef.get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                attendees.push(doc.data());
+            })
+            res.send(attendees);
+			return attendees;
+        })
+        .catch((err) => {
+          console.log('Error getting documents', err);
+        });
 	} else if (req.method === "POST") {
-		res.send("New attendee added");
+        let newAttendee = {
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type
+        };
+
+        attendeesRef.add(newAttendee)
+		.then((ref) => {
+            res.json({
+                id: ref.id,
+                message: "Attendee successfully added"
+            });
+			return newAttendee;
+        })
+		.catch((err) => {
+          console.log('Error adding an attendee', err);
+        });
 	} else if (req.method === "DELETE") {
-		res.send("Addendee deleted");
+        attendeesRef.doc(req.body.id).delete();
+        res.send('Attendee successfully removed');
 	} else {
 		res.status(404).send('Not Found');
 	}
@@ -33,9 +54,36 @@ exports.attendees = functions.https.onRequest((req, res) => {
 
 exports.wishes = functions.https.onRequest((req, res) => {
 	if (req.method === "GET") {
-		res.send("Got all wishes");
+        let wishes = [];
+
+        wishesRef.get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                wishes.push(doc.data());
+            })
+            res.send(wishes);
+			return wishes;
+        })
+        .catch((err) => {
+			console.log('Error getting wishes', err);
+        });
 	} else if (req.method === "POST") {
-		res.send("New wish added");
+        let newWish = {
+            name: req.body.name,
+            wish: req.body.wish
+        };
+
+        wishesRef.add(newWish)
+		.then((ref) => {
+            res.json({
+                id: ref.id,
+                message: "Wish successfully added"
+            });
+			return newWish;
+        })
+		.catch((err) => {
+			console.log('Error adding a wish', err);
+        });
 	} else {
 		res.status(404).send('Not Found');
 	}
